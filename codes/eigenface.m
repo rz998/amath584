b5df451dad70353ccffe4e90960ba3b24ad7ec9b
Gary Zhao <juggsoul@gmail.com>
2020-10-16 17:26:00 +0800

    Update eigenface.m

--- codes/eigenface.m
+++ codes/eigenface.m
@@ -27,8 +27,8 @@ xlabel('$k$','interpreter','latex')
 ylabel('Singular values $\sigma_k$','interpreter','latex')
 set(gca,'Fontsize',[12])
 for r=1:5 % Testing for different r values
-subplot(2,3,r);facel=reshape(U(:,r),[192 168]);imagesc(facel),colormap gray;
-set(gca,'XTick',[], 'YTick', [])
+    subplot(2,3,r);facel=reshape(U(:,r),[192 168]);imagesc(facel),colormap gray;
+    set(gca,'XTick',[], 'YTick', [])
 end
 saveas(gcf,'eigenface.png')
 
@@ -40,12 +40,12 @@ set(gca,'XTick',[], 'YTick', [])
 set(gca,'Fontsize',[12])
 i=1;
 for r=[25 50 100 200 400 800 1600] % Testing for different r values
-Xrec = U(:,1:r)*(U(:,1:r)'*X);
-i=i+1;
-subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
-title(['$r=~$',num2str(r)],'interpreter','latex')
-set(gca,'XTick',[], 'YTick', [])
-set(gca,'Fontsize',[12])
+    Xrec = U(:,1:r)*(U(:,1:r)'*X);
+    i=i+1;
+    subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
+    title(['$r=~$',num2str(r)],'interpreter','latex')
+    set(gca,'XTick',[], 'YTick', [])
+    set(gca,'Fontsize',[12])
 end
 saveas(gcf,'testface1.png')
 
@@ -57,12 +57,12 @@ set(gca,'XTick',[], 'YTick', [])
 set(gca,'Fontsize',[12])
 i=1;
 for r=[25 50 100 200 400 800 1600] 
-Xrec = U(:,1:r)*(U(:,1:r)'*X);
-i=i+1;
-subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
-title(['$r=~$',num2str(r)],'interpreter','latex')
-set(gca,'XTick',[], 'YTick', [])
-set(gca,'Fontsize',[12])
+    Xrec = U(:,1:r)*(U(:,1:r)'*X);
+    i=i+1;
+    subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
+    title(['$r=~$',num2str(r)],'interpreter','latex')
+    set(gca,'XTick',[], 'YTick', [])
+    set(gca,'Fontsize',[12])
 end
 saveas(gcf,'testface2.png')
 
@@ -73,10 +73,10 @@ W = dir(fullfile(Z,'*'));
 X = {W(~[W.isdir]).name};
 D1=[];
 for i = 1:numel(X)
-        F = fullfile(Z,X{i});
-        E=imresize(double(imread(F)),[192 168]);
-        G=reshape(E,[32256,1]);
-        D1=[D1 G];
+    F = fullfile(Z,X{i});
+    E=imresize(double(imread(F)),[192 168]);
+    G=reshape(E,[32256,1]);
+    D1=[D1 G];
 end
 
 trainD1=D1(:,1:143);
@@ -90,12 +90,12 @@ set(gca,'XTick',[], 'YTick', [])
 set(gca,'Fontsize',[12])
 i=1;
 for r=[20 40 60 80 100 120 140] 
-Xrec = U1(:,1:r)*(U1(:,1:r)'*X);
-i=i+1;
-subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
-title(['$r=~$',num2str(r)],'interpreter','latex')
-set(gca,'XTick',[], 'YTick', [])
-set(gca,'Fontsize',[12])
+    Xrec = U1(:,1:r)*(U1(:,1:r)'*X);
+    i=i+1;
+    subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
+    title(['$r=~$',num2str(r)],'interpreter','latex')
+    set(gca,'XTick',[], 'YTick', [])
+    set(gca,'Fontsize',[12])
 end
 saveas(gcf,'testface3.png')
 
@@ -108,12 +108,12 @@ set(gca,'XTick',[], 'YTick', [])
 set(gca,'Fontsize',[12])
 i=1;
 for r=[20 40 60 80 100 120 140] 
-Xrec = U1(:,1:r)*(U1(:,1:r)'*X);
-i=i+1;
-subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
-title(['$r=~$',num2str(r)],'interpreter','latex')
-set(gca,'XTick',[], 'YTick', [])
-set(gca,'Fontsize',[12])
+    Xrec = U1(:,1:r)*(U1(:,1:r)'*X);
+    i=i+1;
+    subplot(2,4,i);imagesc(reshape(Xrec,[192 168])),colormap gray
+    title(['$r=~$',num2str(r)],'interpreter','latex')
+    set(gca,'XTick',[], 'YTick', [])
+    set(gca,'Fontsize',[12])
 end
 saveas(gcf,'testface4.png')
 
